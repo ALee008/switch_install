e63516462e266921d4f5c82872a752938d980287
ALee008 <nomansland008@googlemail.com>
2019-01-01 14:55:19 +0100

    Update switch_install.py
    adding latest update according to url

--- switch_install.py
+++ switch_install.py
@@ -3,6 +3,10 @@ import re
 import csv
 import glob
 import string
+import requests
+from collections import defaultdict
+from bs4 import BeautifulSoup
+
 
 
 class Switch(object):
@@ -13,7 +17,6 @@ class Switch(object):
         self.filepath = path
         self.root, self.dirname = os.path.split(self.filepath)
         self.and_update_tag = '.and.Update.'
-        self.list_of_updates_and_patches_url = 'http://www.benoitren.be/switch-gamepatches.html'
 
     def get_name(self):
         """
@@ -123,21 +126,70 @@ class DLC(Switch):
     dlcs = property(get_dlcs)
 
 
+class LatestUpdate(object):
+
+    def __init__(self):
+        self.list_of_updates_and_patches_url = 'http://www.benoitren.be/switch-gamepatches.html'
+
+    @staticmethod
+    def format_game_name(game_name):
+        """
+
+        :param game_name:
+        :return:
+        """
+        formatted_game_name = game_name.replace(' ', '.').replace(':', '.').replace('..', '.')
+        formatted_game_name = formatted_game_name.replace('\'', '')
+        formatted_game_name = formatted_game_name.replace('!', '')
+        formatted_game_name = formatted_game_name.replace('.(USA)', '').replace('.(Europe)', '')
+        formatted_game_name = formatted_game_name.replace('.(physical)', '').replace('.(digital)', '')
+
+        if ',.The'.upper() in formatted_game_name.upper():
+            formatted_game_name = formatted_game_name.replace(',.The', '')
+            formatted_game_name = 'The.' + formatted_game_name
+
+        return formatted_game_name
+
+    def get_list_of_updates(self):
+        """
+
+        :return:
+        """
+        res = defaultdict(list)
+        page = requests.get(self.list_of_updates_and_patches_url)
+        html_doc = page.content
+        soup = BeautifulSoup(html_doc, 'html.parser')
+
+        tr_tags = soup.tbody.find_all('tr')
+
+        for tr_block in tr_tags:
+            # filter empty tags, so called NavigableString. Because last item is truly empty it will be consumed.
+            try:
+                game_name, latest_update, update_date, _ = filter(lambda x:x.name, tr_block.contents)
+                game_name = self.format_game_name(game_name.text)
+                res[game_name].extend([latest_update.text, update_date.text])
+            except ValueError:
+                print 'WARNING: Skipping game %s. Row seems faulty.' %tr_block.td.text
+
+        return res
+
+    list_of_updates = property(get_list_of_updates)
+
+
 def get_game_infos(filepath):
     """
     Gather game info for later export.
     :param filepath: (str) path where games are located.
     :return: (tuple) game infos
     """
+    game_infos = defaultdict(list)
     game = Game(filepath)
 
     print "Processing game %s ..." % game.game_name
-    game_infos = (game.game_name, game.short_name, game.extension,
-                  game.update.update_versions, game.dlc.dlcs)
+    game_infos[game.game_name].extend([game.short_name, game.extension,
+                                       game.update.update_versions, game.dlc.dlcs])
 
-    print game_infos
-
-    return game_infos
+    return game_infos, game.game_name
 
 
 def run_collect_game_infos(games_path):
@@ -150,12 +202,15 @@ def run_collect_game_infos(games_path):
                       if os.path.isdir(game)
                       and not os.path.basename(game).startswith('_DLC')]
 
-    res = []
+    game_infos = []
+    game_names = []
 
     for game_path in games_abs_path:
-        res.append(get_game_infos(game_path))
+        game_info, game_name = get_game_infos(game_path)
+        game_infos.append(game_info)
+        game_names.append(game_name)
 
-    return res
+    return game_infos, game_names
 
 
 def write_to_file(outfile, result_list):
@@ -175,6 +230,17 @@ def write_to_file(outfile, result_list):
     return None
 
 
-_path = r'\\KH-NAS\Sicherungen\Games\Switch'#r'C:\temp'
-t = run_collect_game_infos(_path)
-write_to_file(r'C:\temp\test.csv', t)
+test = LatestUpdate()
+_path = r'\\KH-NAS\Sicherungen\Games\Switch'#r'C:\temp'#
+game_infos, game_names = run_collect_game_infos(_path)
+game_from_online_list = map(lambda x:x.upper(), test.list_of_updates.keys())
+
+i = 1
+for game in game_names:
+    if game.upper() in game_from_online_list:
+        #print 'Found game %s (%d/%d).' % (game, i, len(game_names))
+        i += 1
+    else:
+        print 'WARNING. Match not found. Skipping game %s.' % game
+
+#write_to_file(r'C:\temp\test.csv', t)
